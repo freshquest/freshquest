0f37d0add63f826417260e21f3d67fdfd83204ac
Jacob Beard <jbeard4@cs.mcgill.ca>
2013-04-13 18:24:50 -0400

    Added REST API layer. Seems to work well.

--- index.js
+++ index.js
@@ -3,28 +3,30 @@
 
 //TODO: later, add auth, etc.
 
-var express = require("express");
+var express = require("express"),
+    rest = require('./rest');
+
+
 var app = express();
+app.use(express.bodyParser());
 app.use(express.logger());
 
 app.use(express['static'](__dirname + '/public'));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
 
 var mongo = require('mongodb');
 
 var mongoUri = process.env.MONGOLAB_URI || 
-  process.env.MONGOHQ_URL || 
-  'mongodb://localhost/mydb'; 
+    process.env.MONGOHQ_URL || 
+    'mongodb://heroku:mintyfresh@alex.mongohq.com:10063/app14931700';
+
+//TODO: autoreconnect
 
 mongo.Db.connect(mongoUri, function (err, db) {
-  console.log('connected to mongodb');
-  //db.collection('mydocs', function(er, collection) {
-  //  collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
-  //  });
-  //});
+    console.log('connected to mongodb');
+    rest(app,db);
 });
-
---
+++ rest.js
@@ -0,0 +1,140 @@
+var mongo = require("mongodb"),
+    BSON = mongo.BSONPure;
+ 
+module.exports = function setupRest(app, db) {
+
+    function sendError(status,message,res){
+        res.header('Content-Type', 'application/json');
+        res.send({error : message},status); 
+    }
+ 
+    /**
+     * Query
+     */
+    app.get('/api/:collection/:id?', function (req, res) {
+        var query = req.query.query ? JSON.parse(req.query.query) : {};
+ 
+        // Providing an id overwrites giving a query in the URL
+        if (req.params.id) {
+            query = {
+                '_id': new BSON.ObjectID(req.params.id)
+            };
+        }
+        var options = req.params.options || {};
+ 
+        var test = ['limit', 'sort', 'fields', 'skip', 'hint', 'explain', 'snapshot', 'timeout'];
+ 
+        for (var o in req.query) {
+            if (test.indexOf(o) >= 0) {
+                options[o] = req.query[o];
+            }
+        }
+ 
+        db.collection(req.params.collection, function (err, collection) {
+
+            if(!collection) return res.send("Unable to find collection",400,res);
+            if(err) return sendError(err.message,500,res);
+
+            collection.find(query, options, function (err, cursor) {
+
+                if(!cursor) return res.send("Unable to execute query",400,res);
+                if(err) return sendError(err.message,500,res);
+ 
+                cursor.toArray(function (err, docs) {
+
+                    if(!docs) return sendError('Unable to convert database cursor to array',500,res);
+                    if(err) return sendError(err.message,500,res);
+
+                    var result = [];
+                    if (req.params.id) {
+                        if (docs.length > 0) {
+                            result = JSON.stringify(docs[0]);
+                            res.header('Content-Type', 'application/json');
+                            res.send(result);
+                        } else {
+                            res.send(404);
+                        }
+                    } else {
+                        docs.forEach(function (doc) {
+                            result.push(JSON.stringify(doc));
+                        });
+                        res.header('Content-Type', 'application/json');
+                        res.send(result);
+                    }
+                });
+            });
+        });
+    });
+ 
+    /**
+     * Insert
+     */
+    app.post('/api/:collection', function (req, res) {
+        if (!req.body) return sendError('You must specify a collection to insert',400,res);
+
+        db.collection(req.params.collection, function (err, collection) {
+
+            if(!collection) return res.send("Unable to find collection",400,res);
+            if(err) return sendError(err.message,500,res);
+
+            // We only support inserting one document at a time
+            collection.insert(Array.isArray(req.body) ? req.body[0] : req.body, function (err, docs) {
+
+                if(!docs) return res.send("Unable to insert document into collection",400,res);
+                if(err) return sendError(err.message,500,res);
+
+                res.header('Location', '/' + req.params.db + '/' + req.params.collection + '/' + docs[0]._id.toHexString());
+                res.header('Content-Type', 'application/json');
+                res.send(JSON.stringify(docs), 201);
+            });
+        });
+    });
+ 
+    /**
+     * Update
+     */
+    app.put('/api/:collection/:id', function (req, res) {
+        var spec = {
+            '_id': new BSON.ObjectID(req.params.id)
+        };
+ 
+        db.collection(req.params.collection, function (err, collection) {
+
+            if(!collection) return res.send("Unable to find collection",400,res);
+            if(err) return sendError(err.message,500,res);
+
+            collection.update(spec, req.body, true, function (err, docs) {
+
+                if(!docs) return res.send("Unable to update document",400,res);
+                if(err) return sendError(err.message,500,res);
+
+                res.header('Content-Type', 'application/json');
+                res.send(JSON.stringify(docs));
+            });
+        });
+    });
+ 
+    /**
+     * Delete
+     */
+    app.del('/api/:collection/:id', function (req, res) {
+        var spec = {
+            '_id': new BSON.ObjectID(req.params.id)
+        };
+ 
+        db.collection(req.params.collection, function (err, collection) {
+
+            if(!collection) return res.send("Unable to find collection",400,res);
+            if(err) return sendError(err.message,500,res);
+
+            collection.remove(spec, function (err, docs) {
+
+                if(!docs) return res.send("Unable to update document",400,res);
+                if(err) return sendError(err.message,500,res);
+
+                res.header('Content-Type', 'application/json');
+                res.send(JSON.stringify(docs));
+            });
+        });
+    });
+};
